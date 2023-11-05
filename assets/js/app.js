ccf918563aa7a0b7eb1bc985c25bf1031df0c12b
André Cardoso dos Santos <andrecdsantos0@gmail.com>
2023-11-05 15:30:03 -0300

    parte desktop feita, faltando a parte menu mobile

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+import { title } from "/config/config.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('h1').innerText = title;
+  
+    const sectionComunidade = document.querySelector('.section__comunidade');
+    const section = document.querySelector('section');
+    sectionComunidade.style.width = section.offsetWidth + "px";
+    sectionComunidade.style.height = section.offsetWidth + "px";
+    
+    window.addEventListener('resize', () => {
+        sectionComunidade.style.width = section.offsetWidth + "px";
+        sectionComunidade.style.height = section.offsetWidth + "px";
+    })
+});
+
+let aberto = false; // Alterei para 'let' para permitir a mudança de valor
+const menuMobile = document.querySelector('.menu__mobile');
+const menu = document.querySelector('.menu');
+
+menuMobile.addEventListener('click', () => {
+    aberto = !aberto; // Inverte o valor de 'aberto' entre true e false
+    menuMobile.classList.toggle('ativado');
+    menu.classList.toggle('ativado');
+    aberto ? (menuMobile.innerHTML = 'X') : (menuMobile.innerHTML = '&#9776;');
+});
---
+++ config/config.js
@@ -0,0 +1,7 @@
+const title = 'Educate';
+
+/* document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('h1').innerText = title;
+}); */
+
+export { title };
